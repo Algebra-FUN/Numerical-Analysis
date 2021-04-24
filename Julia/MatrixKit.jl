3a6b73904e697f9be89546146741ccad48926781
Algebra-FUN <worldcreator@foxmail.com>
2021-04-24 10:01:06 +0800

    update

--- Julia/MatrixKit.jl
+++ Julia/MatrixKit.jl
@@ -6,19 +6,17 @@ eye(n) = Matrix{Float64}(I,n,n)
 function matrix2latex(M)
     rows_str = @λ(join(M[_,:],'&')) |> @λ map(_,1:size(M)[1])
     matrix_str = join(rows_str,"\\\\")
-    return "\\begin{bmatrix}$(matrix_str)\\end{bmatrix}"
+    return "\\begin{bmatrix}$matrix_str\\end{bmatrix}"
 end
 
-function matrix_form(name::String,M)
-    return "<center>``"*name*"="*matrix2latex(M)*"``</center>"
-end
+centralize(content::String) = "<center>$content</center>"
 
-function matrix_form(symbol::Symbol,M)
-    return matrix_form(String(symbol),M)
-end
+matrix_form(name::String,M) = "``"*name*"="*matrix2latex(M)*"``"
+
+matrix_form(symbol::Symbol,M) = matrix_form(String(symbol),M)
 
 macro latex(M::Symbol)
-    return matrix_form(M,M |> eval) |> Markdown.parse
+    return matrix_form(M,M |> eval) |> centralize |> Markdown.parse
 end
 
 macro latex(expr::Expr)
@@ -27,7 +25,7 @@ macro latex(expr::Expr)
         right = expr.args[2] |> eval
         if typeof(left) == Symbol && typeof(right) in (Array{Float64,2},Vector{Float64})
             expr |> eval
-            return matrix_form(left,right) |> Markdown.parse
+            return matrix_form(left,right) |> centralize |> Markdown.parse
         end
     end
     return expr
